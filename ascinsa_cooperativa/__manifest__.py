a4d0e2f1d866673976da41030da5b2a0990f88a8
Jean Carlo <jeancarlo.delacruz@ascinsa.com.pe>
2022-02-11 22:33:32 +0000

    [ADD] Modulo de Cooperativa

---
+++ ascinsa_cooperativa/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Ascinsa Cooperativa',
+    
+    'summary' : """Cooperativa administra los procesos de una tienda de voluntarios""",
+    
+    'description' : """
+        Modulo Cooperativo administra:
+        - Voluntarios
+        - Productos
+        - Tienda
+    """,
+    
+    'author' : 'Ascinsa', 
+    
+    'website' : 'https://www.teleservicio.com', 
+    
+    'category' : 'Administration', 
+    
+    'version' : '0.1', 
+    
+    'depends' : ['base'], 
+    
+    'data' : [
+        
+    ],
+    
+    'demo' : [
+        
+    ],
+}
