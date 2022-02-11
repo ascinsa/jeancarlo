fa8f58e978d105ff4520f93f923ef16e28fc2321
Jean Carlo <jeancarlo.delacruz@ascinsa.com.pe>
2022-02-11 20:44:51 +0000

    [ADD] Modulo de gestión de biblioteca

---
+++ ascinsa_biblioteca/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Ascinsa Biblioteca', 
+    
+    'summary' : """Gestión y Administración de una Biblioteca"""
+    
+    'description' : """
+        Modulo de Biblioteca que nos permitira:
+        - Administrar libros.
+        - Administrar clientes.
+        - Organizar alquileres.
+    """,
+    
+    'author' : 'ascinsa', 
+    
+    'website' : 'http://www.teleservicio.com', 
+    
+    'category' : 'administration', 
+    
+    'version' : '1.0.0.1', 
+    
+    'depends' : ['base'], 
+    
+    'data' : [
+        
+    ], 
+    
+    'demo' : [
+        
+    ]
+}
