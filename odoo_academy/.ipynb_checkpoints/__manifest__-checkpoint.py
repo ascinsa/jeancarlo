c72b4d319bc8ca2eab917a5905f5566a998188ef
juan carlos <github@ascinsa.net>
2022-02-11 16:28:15 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Academy',
+    
+    'summary' : """Academy app to manage Trainning""",
+    
+    'description' : """
+        Academy Module to manage Trainning:
+        - Courses
+        - Sessions
+        - Attendees
+    """
+}
