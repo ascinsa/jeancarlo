5719a136b21992b7ca61151eaf657566b71c5297
juan carlos <github@ascinsa.net>
2022-02-11 19:05:42 +0000

    [ADD] Registro de Modulo Espacial

---
+++ odoo_espacial/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# # -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Espacial', 
+    
+    'summary' : """Administración de proyectos espaciales""", 
+    
+    'description' : """
+        Modulo Espacial nos permitirá administrar:
+        - Proyectos espaciales.
+        - Organizar Logistica.
+        - Planificación de viajes espaciales.
+    """, 
+    
+    'author' : 'Ascinsa', 
+    
+    'website' : 'https://www.ascinsa.com.pe', 
+    
+    'category' : 'administration', 
+    
+    'version' : '1.0.0.1', 
+    
+    'depends' : ['base'], 
+    
+    'data' : [
+        
+    ], 
+    
+    'demo' : [
+        
+    ],
+}
